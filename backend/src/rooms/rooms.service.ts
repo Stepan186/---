cbb5e31c2f49c8b5b6abe1a7e9c0d77809b8ea65
Степан <kokorin121@yandex.ru>
2023-07-15 05:19:17 +0500

    .

--- backend/src/rooms/rooms.service.ts
+++ backend/src/rooms/rooms.service.ts
@@ -13,13 +13,19 @@ export class RoomsService {
     ) {
     }
 
+
     async getMany(dto: GetRoomsDto) {
-        return await this.em.execute(`select r.*
-                                      from booking b
-                                               left join room r on b.room_id = r.id
-                                      where (b.check_in, b.check_out) OVERLAPS
-                                            (to_timestamp(${ dto.checkIn } / 1000),
-                                             to_timestamp(${ dto.checkOut } / 1000)) != true
-                                      group by r.id;`);
+        return await this.em.execute(`select *
+                                      from room
+                                      where id not in (select room.id
+                                                       from room
+                                                                left join booking b on room.id = b.room_id
+                                                       where (to_timestamp(${dto.checkIn}) > b.check_in and to_timestamp(${dto.checkIn}) < b.check_out)
+                                                          or (to_timestamp(${dto.checkOut}) > b.check_in and to_timestamp(${dto.checkOut}) < check_out)
+                                                          or (to_timestamp(${dto.checkIn}) < check_in and to_timestamp(${dto.checkOut}) > check_out));`);
     }
 }
+
+// select r.* from room r
+// left join booking b on r.id = b.room_id
+// except  (select r2.* from room r2 left join booking b2 on r2.id = b2.room_id where (b2.check_in, b2.check_out) OVERLAPS (${checkIn},${checkOut}) is true group by r2.id);
